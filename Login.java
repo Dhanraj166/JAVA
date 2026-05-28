788e7c1bba927bc41fae0ff5680029d8c3d40f8a
Dhanraj166 <dhanrajs781004@gmail.com>
2026-05-28 23:59:31 +0530

    first commit

---
+++ Login.java
@@ -0,0 +1,25 @@
+public class Login {
+    public static void main(String[] args) {
+
+        byte age = 25;
+        short year = 2025;
+        int salary = 75000;
+        long Population = 8000000000L;
+
+        float height = 5.9f;
+        double pi = 3.14159265358979;
+
+        char grade = 'A';
+
+        boolean isStudent = false;
+
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Salary: " + salary);
+        System.out.println("World Population: " + Population);
+        System.out.println("Height: " + height);
+        System.out.println("Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
