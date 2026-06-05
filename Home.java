22839796160e0a001fffc1d4816b797e181cbce6
Dhanraj166 <dhanrajs781004@gmail.com>
2026-06-05 23:57:40 +0530

    inheritance practice

--- Home.java
+++ Home.java
@@ -1,14 +1,23 @@
 import mypack.Practice;
-public class Home{
-    public static void main(String[] args){
-        Practice name = new Practice();
-        name.hello();
-        name.bye();
-    }
-}
+// public class Home{
+
+
+//     pu
+//     public static void main(String[] args){
+//         Practice name = new Practice();
+//         name.hello();
+//         name.bye();
+//     }
+// }
 
 
 public class Home extends Practice{
+
+public Home (){
+    System.out.println("Hey Buddy");
+}
+    
+
     public static void main(String[] args){
         Home name = new Home();
         name.hello();
--- Practice.java
+++ Practice.java
@@ -1,9 +1,16 @@
 package mypack;
 public class Practice{
+
+    // String naam;
+
+
+    public Practice(){System.out.println("Hey");}
     public void hello(){
         System.out.println("Hello method ... ");
     }
     public void bye(){
         System.out.println("Bye method ... ");
     }
+
+    // Practice practice = new Practice("  heirhio");
 }
