dcb95f3dcdc2977e10be55f9029cb18daf19ad88
Dhanraj166 <dhanrajs781004@gmail.com>
2026-07-02 23:23:04 +0530

    Passing Objects to Methods

---
+++ Laptop.java
@@ -0,0 +1,45 @@
+public class Laptop {
+
+    int ram;          
+    String processor;
+    int storage;      
+    int price;        
+
+    public Laptop(int ram, String processor, int storage, int price) {
+        this.ram = ram;
+        this.processor = processor;
+        this.storage = storage;
+        this.price = price;
+    }
+
+    public Laptop() {
+    }
+
+    public static void main(String[] args) {
+
+        Laptop l1 = new Laptop(8, "Intel i5", 512, 60);
+        Laptop l2 = new Laptop(16, "Intel i7", 1024, 85);
+
+        l2.compareLaptop(l1);
+    }
+
+    public void compareLaptop(Laptop l1) {
+
+        System.out.println("Laptop 1 Price: " + this.price);
+        System.out.println("Laptop 2 Price: " + l1.price);
+
+        if (this.price > l1.price) {
+            System.out.println("Laptop 1 is more expensive");
+        } else if (this.price < l1.price) {
+            System.out.println("Laptop 2 is more expensive");
+        } else {
+            System.out.println("Both laptops have the same price");
+        }
+
+        if (this.ram > l1.ram) {
+            System.out.println("Laptop 1 has more RAM");
+        } else {
+            System.out.println("Laptop 2 has more RAM");
+        }
+    }
+}
--- SelectionSort.java
+++ SelectionSort.java
@@ -6,7 +6,7 @@ public class SelectionSort {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     int temp = arr[i];
-                    arr[i] = arr[j];
+                    arr[i] = arr[j]; 
                     arr[j] = temp;
                 }
             }
@@ -15,5 +15,6 @@ public class SelectionSort {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+
     }
 }
